151f8916fe754676126eaa7fa159361aa6542096
Juanparra <juan.parra1.mt@correo.usa.edu.co>
2021-10-30 09:05:57 -0500

    reto 5 backend

---
+++ src/main/java/com/ciclo3/juanparra/repository/crud/ReservationCrudRepository.java
@@ -0,0 +1,18 @@
+package com.ciclo3.juanparra.repository.crud;
+
+import java.util.Date;
+import java.util.List;
+import com.ciclo3.juanparra.model.Reservation;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservationCrudRepository extends CrudRepository <Reservation,Integer> {
+    public List<Reservation> findAllByStatus (String status); 
+    
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+    
+    // SELECT clientid, COUNT(*) AS total FROM reservacion group by clientid order by desc;
+    @Query ("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationByClient();
+
+}
